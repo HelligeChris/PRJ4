3332cb5c3ac1f1a0ad0fcf457ad1b2cba029f8be
JipJapJeppe <55808892+JipJapJeppe@users.noreply.github.com>
2020-12-07 11:35:30 +0100

    rettet kommentarer
    indhakket og slettet nogle kommentarer

--- POPE/POPEproject.cydsn/kadenceControl.c
+++ POPE/POPEproject.cydsn/kadenceControl.c
@@ -7,17 +7,12 @@
 void MPU6050_init() {
     devAddr = MPU6050_DEFAULT_ADDRESS;
 }
-
 /** Specific address constructor.
  * @param address I2C address
  * @see MPU6050_DEFAULT_ADDRESS
  * @see MPU6050_ADDRESS_AD0_LOW
  * @see MPU6050_ADDRESS_AD0_HIGH
  */
-//void I2C_MPU6050_I2CAddress(uint8_t address) {
-//    devAddr = address;
-//}
-
 /** Power on and prepare for general usage.
  * This will activate the device and take it out of sleep mode (which must be done
  * after start-up). This function also sets both the accelerometer and the gyroscope
@@ -31,11 +26,6 @@ void MPU6050_initialize() {
 //    MPU6050_setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
     MPU6050_setSleepEnabled(false); // thanks to Jack Elston for pointing this one out!
 }
-
-//uint8_t MPU6050_getDLPFMode() {
-//    I2CReadBits(devAddr, MPU6050_RA_CONFIG, MPU6050_CFG_DLPF_CFG_BIT, MPU6050_CFG_DLPF_CFG_LENGTH, buffer);
-//    return buffer[0];
-//}
 /** Set digital low-pass filter configuration.
  * @param mode New DLFP configuration setting
  * @see getDLPFBandwidth()
@@ -47,9 +37,7 @@ void MPU6050_initialize() {
 void MPU6050_setDLPFMode(uint8_t mode) {
     I2CWriteBits(devAddr, MPU6050_RA_CONFIG, MPU6050_CFG_DLPF_CFG_BIT, MPU6050_CFG_DLPF_CFG_LENGTH, mode);
 }
-
 // GYRO_CONFIG register
-
 /** Get full-scale gyroscope range.
  * The FS_SEL parameter allows setting the full-scale range of the gyro sensors,
  * as described in the table below.
@@ -82,67 +70,54 @@ uint8_t MPU6050_getFullScaleGyroRange() {
 void MPU6050_setFullScaleGyroRange(uint8_t range) {
     I2CWriteBits(devAddr, MPU6050_RA_GYRO_CONFIG, MPU6050_GCONFIG_FS_SEL_BIT, MPU6050_GCONFIG_FS_SEL_LENGTH, range);
 }
-
-//void MPU6050_getRotation(int16_t* x, int16_t* y, int16_t* z) {
-//    I2CReadBytes(devAddr, MPU6050_RA_GYRO_XOUT_H, 6, buffer);
-//    *x = (((int16_t)buffer[0]) << 8) | buffer[1];
-//    *y = (((int16_t)buffer[2]) << 8) | buffer[3];
-//    *z = (((int16_t)buffer[4]) << 8) | buffer[5];
-//}
-///** Get X-axis gyroscope reading.
-// * @return X-axis rotation measurement in 16-bit 2's complement format
-// * @see getMotion6()
-// * @see MPU6050_RA_GYRO_XOUT_H
-// */
+/** Get X-axis gyroscope reading.
+ * @return X-axis rotation measurement in 16-bit 2's complement format
+ * @see getMotion6()
+ * @see MPU6050_RA_GYRO_XOUT_H
+ */
 int16_t MPU6050_getRotationX() {
     I2CReadBytes(devAddr, MPU6050_RA_GYRO_XOUT_H, 2, buffer);
     return (((int16_t)buffer[0]) << 8) | buffer[1];
 }
-
-//bool MPU6050_getSleepEnabled() {
-//    I2CReadBit(devAddr, MPU6050_RA_PWR_MGMT_1, MPU6050_PWR1_SLEEP_BIT, buffer);
-//    return buffer[0];
-//}
-///** Set sleep mode status.
-// * @param enabled New sleep mode enabled status
-// * @see getSleepEnabled()
-// * @see MPU6050_RA_PWR_MGMT_1
-// * @see MPU6050_PWR1_SLEEP_BIT
-// */
+/** Set sleep mode status.
+ * @param enabled New sleep mode enabled status
+ * @see getSleepEnabled()
+ * @see MPU6050_RA_PWR_MGMT_1
+ * @see MPU6050_PWR1_SLEEP_BIT
+ */
 void MPU6050_setSleepEnabled(bool enabled) {
     I2CWriteBit(devAddr, MPU6050_RA_PWR_MGMT_1, MPU6050_PWR1_SLEEP_BIT, enabled);
 }
-
-///** Set clock source setting.
-// * An internal 8MHz oscillator, gyroscope based clock, or external sources can
-// * be selected as the MPU-60X0 clock source. When the internal 8 MHz oscillator
-// * or an external source is chosen as the clock source, the MPU-60X0 can operate
-// * in low power modes with the gyroscopes disabled.
-// *
-// * Upon power up, the MPU-60X0 clock source defaults to the internal oscillator.
-// * However, it is highly recommended that the device be configured to use one of
-// * the gyroscopes (or an external clock source) as the clock reference for
-// * improved stability. The clock source can be selected according to the following table:
-// *
-// * <pre>
-// * CLK_SEL | Clock Source
-// * --------+--------------------------------------
-// * 0       | Internal oscillator
-// * 1       | PLL with X Gyro reference
-// * 2       | PLL with Y Gyro reference
-// * 3       | PLL with Z Gyro reference
-// * 4       | PLL with external 32.768kHz reference
-// * 5       | PLL with external 19.2MHz reference
-// * 6       | Reserved
-// * 7       | Stops the clock and keeps the timing generator in reset
-// * </pre>
-// *
-// * @param source New clock source setting
-// * @see getClockSource()
-// * @see MPU6050_RA_PWR_MGMT_1
-// * @see MPU6050_PWR1_CLKSEL_BIT
-// * @see MPU6050_PWR1_CLKSEL_LENGTH
-// */
+/** Set clock source setting.
+ * An internal 8MHz oscillator, gyroscope based clock, or external sources can
+ * be selected as the MPU-60X0 clock source. When the internal 8 MHz oscillator
+ * or an external source is chosen as the clock source, the MPU-60X0 can operate
+ * in low power modes with the gyroscopes disabled.
+ *
+ * Upon power up, the MPU-60X0 clock source defaults to the internal oscillator.
+ * However, it is highly recommended that the device be configured to use one of
+ * the gyroscopes (or an external clock source) as the clock reference for
+ * improved stability. The clock source can be selected according to the following table:
+ *
+ * <pre>
+ * CLK_SEL | Clock Source
+ * --------+--------------------------------------
+ * 0       | Internal oscillator
+ * 1       | PLL with X Gyro reference
+ * 2       | PLL with Y Gyro reference
+ * 3       | PLL with Z Gyro reference
+ * 4       | PLL with external 32.768kHz reference
+ * 5       | PLL with external 19.2MHz reference
+ * 6       | Reserved
+ * 7       | Stops the clock and keeps the timing generator in reset
+ * </pre>
+ *
+ * @param source New clock source setting
+ * @see getClockSource()
+ * @see MPU6050_RA_PWR_MGMT_1
+ * @see MPU6050_PWR1_CLKSEL_BIT
+ * @see MPU6050_PWR1_CLKSEL_LENGTH
+ */
 void MPU6050_setClockSource(uint8_t source) {
     I2CWriteBits(devAddr, MPU6050_RA_PWR_MGMT_1, MPU6050_PWR1_CLKSEL_BIT, MPU6050_PWR1_CLKSEL_LENGTH, source);
 }
